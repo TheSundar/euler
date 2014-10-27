7c1e75a824f5383c0690c1457c1c3ac4c085ea17
Sundara Nataraja <iamsundar04@gmail.com>
2014-10-27 12:02:38 +0530

    Create problem43.py

---
+++ problem43.py
@@ -0,0 +1,9 @@
+import itertools
+lis=[2,3,5,7,11,13,17]
+sum1=0
+for i in itertools.permutations('0123456789',10):
+    i=''.join(i)
+    if all([int(i[j[0]:j[0]+3])%j[1]==0 for j in zip(range(1,8),lis)]):
+        print i
+        sum1+=int(i)
+print sum1
